d2a6a423af7d66d09786f70f5608e6142296b1f8
re9ant <re9ant@gmail.com>
2022-06-15 23:28:34 +0530

    Added Pet and 69 other changes

---
+++ Project007/Assets/Scripts/PetController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PetController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent navAgent;
+    private Animator myAnimator;
+
+    private int speed_Hash;
+
+    private void Awake()
+    {
+        try
+        {
+            navAgent = GetComponent<NavMeshAgent>();
+        }
+        catch
+        {
+            Debug.LogError("NO NAV MESH AGENT COMP IN PET");
+        }
+        
+        try
+        {
+            myAnimator = GetComponent<Animator>();
+        }
+        catch
+        {
+            Debug.LogError("NO Animator COMP IN PET");
+        }
+    }
+
+    private void Start()
+    {
+        speed_Hash = Animator.StringToHash("Speed");
+    }
+
+    private void Update()
+    {
+        ManageAnimation();
+        ManageNavigation();
+    }
+
+    private void ManageAnimation()
+    {
+        myAnimator.SetFloat(speed_Hash, navAgent.velocity.magnitude);
+    }
+
+    private void ManageNavigation()
+    {
+        float remainingDistance = Vector3.Distance(transform.position, target.position);
+        if (remainingDistance < 0.3f)
+        {
+            navAgent.isStopped = true;
+        }
+        else
+        {
+            navAgent.isStopped = false;
+        }
+        navAgent.SetDestination(target.position);
+    }
+
+    private void OnAnimatorMove()
+    {
+        navAgent.speed = myAnimator.deltaPosition.magnitude * Time.deltaTime;
+    }
+}
